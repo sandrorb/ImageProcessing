4c51cb7638fb5c6d2c99a8c3aa2975410191cf99
sandrorb <sandrorb@gmail.com>
2013-03-13 23:20:12 -0300

    Creating square and circle in a jpeg image.

--- src/ImageProcessing.cpp
+++ src/ImageProcessing.cpp
@@ -24,11 +24,42 @@ int main(int argc, char **argv) {
 	try {
 		Image image;
 		image.read(fileNameFullPath);
-		image.display(); // display the image in an X11 window.
+
+		// put a 10x10 blue square at the point (50,50)
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				image.pixelColor( 50 + i, 50 + j, "blue" );
+			}
+		}
+
+		// Making a red circle
+		const double PI = 3.141592653589793238462;
+		double angle = 0.0;
+		double radius = 20.0;
+		const int centerX = 55;
+		const int centerY = 55;
+		do {
+			int x = radius * cos(angle);
+			int y = radius * sin(angle);
+			image.pixelColor(centerX + x, centerY + y, "red");
+			angle = angle + 0.01;
+		} while (angle < 2*PI);
+
+		// display the image in an X11 window and the program stays here until the window is closed.
+		image.display();
+
+//		Pixels pixels(image);
+//		*(pixels.get( 108, 94, 10 ,10 )) = Color("green");
+//		pixels.sync();
+//
+//		image.write(fileNameFullPath);
+//		image.display();
+
 	} catch(std::exception const& e){
-		cout << "There was a error while opening the file : "<< endl;
+		cout << "There was an error while opening the file : "<< endl;
 		cout << e.what() << endl;
 	}
+
 
 	return 0;
 }
